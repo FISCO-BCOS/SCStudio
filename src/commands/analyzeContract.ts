16bdcd6b505c175ff8ef88ac6cfacdc9d564932b
renardbebe <352600801@qq.com>
2020-12-09 16:00:48 +0800

    add generateReport module

--- src/commands/analyzeContract.ts
+++ src/commands/analyzeContract.ts
@@ -1,24 +1,10 @@
 import * as vscode from 'vscode'
 import { getFileContent } from '../utils/getFileContent'
 import { getRequest, postRequest } from '../utils/httpUtils'
-import fs = require('fs')
-import { detailItem, detailPrefix, detailSuffix, tablePrefix, tableSuffix, tableItem } from './ReportTemplate';
+import { checkPlatform, updateDiagnostics } from '../utils/generateReports'
 
 const os = require('os')
 const { window } = vscode
-
-// Windows OS hack
-function checkPlatform(
-    FILEPATH: string
-): string {
-    if (os.platform() === 'win32') {
-        FILEPATH = FILEPATH.replace(/\\/g, '/')
-        if (FILEPATH.charAt(0) === '/') {
-            FILEPATH = FILEPATH.substr(1)
-        }
-    }
-    return FILEPATH;
-}
 
 export async function analyzeContract(
     diagnosticCollection: vscode.DiagnosticCollection,
@@ -118,96 +104,3 @@ export async function analyzeContract(
     );
 }
 
-function updateDiagnostics(document: vscode.TextDocument | undefined, collection: vscode.DiagnosticCollection, obj:any, fileUri:any): void {
-    let diagnostics: vscode.Diagnostic[] = [];
-    let FILEPATH = checkPlatform(fileUri.fsPath);
-    
-    // console.log('FILEPATH  '+FILEPATH)
-    let htmlPath = FILEPATH;
-    let datetime = new Date();
-    var y = datetime.getFullYear();
-    var m = datetime.getMonth()+1;
-    var d = datetime.getDate();
-    var h = datetime.getHours();
-    var mm = datetime.getMinutes();
-    var s = datetime.getSeconds();
-    let dateString = y.toString() + m.toString() + d.toString() + '_' + h.toString() + mm.toString() + s.toString();
-    FILEPATH = FILEPATH.substring(0,FILEPATH.lastIndexOf('/')) + '/vulnerabilitiesInfo_' + dateString + '.txt';
-    htmlPath = htmlPath.substring(0,FILEPATH.lastIndexOf('/')) + '/vulnerabilitiesReport_' + dateString + '.html';
-    if (document) {
-        // console.log(document.uri)
-        vscode.languages.getDiagnostics(document.uri).slice(1,1);
-        obj = JSON.parse(JSON.stringify(obj.text));
-        let json_res = JSON.parse(obj);
-        console.log(json_res);
-        
-        // the HTML page
-        let detailHtml = detailPrefix;
-        let tableHtml = tablePrefix;
-
-        for(var ent in json_res.vulnerabilities) {
-            // console.log(ent)
-            if(json_res.vulnerabilities[ent]) {
-                let message = `Name: ` + json_res.vulnerabilities[ent].name;
-                // write the detail information into the file
-                let details = `Name:` + json_res.vulnerabilities[ent].name + '\n' +
-                    `Description:` + json_res.vulnerabilities[ent].description + '\n' +
-                    `SwcID:` + json_res.vulnerabilities[ent].swcId + '\n' +
-                    `Advice:` + json_res.vulnerabilities[ent].advice + '\n' +
-                    `Line:` + json_res.vulnerabilities[ent].lineNo[0] + '\n' +
-                    `Level:` + json_res.vulnerabilities[ent].level + '\n' + '\n';
-
-                // Generate the detail html sector of the current vulnerability
-                let curDetail=detailItem.replace(/!NAME!/, json_res.vulnerabilities[ent].name);
-                curDetail = curDetail.replace(/!DESCRIPTION!/, json_res.vulnerabilities[ent].description);
-                curDetail = curDetail.replace(/!SWCID!/, json_res.vulnerabilities[ent].swcId);
-                curDetail = curDetail.replace(/!ADVICE!/, json_res.vulnerabilities[ent].advice);
-                curDetail = curDetail.replace(/!LINE!/, json_res.vulnerabilities[ent].lineNo[0]);
-                curDetail = curDetail.replace(/!LEVEL!/, json_res.vulnerabilities[ent].level);
-                curDetail = curDetail.replace(/SOURCECODE/, document.lineAt(json_res.vulnerabilities[ent].lineNo[0]-1).text);
-                detailHtml = detailHtml + curDetail;
-
-                // Generate the table html sector of the current vulnerability
-                let tableItemHtml = tableItem.replace(/!NAME!/, json_res.vulnerabilities[ent].name);
-                tableItemHtml = tableItemHtml.replace(/!LINE!/, json_res.vulnerabilities[ent].lineNo[0]);
-                tableItemHtml = tableItemHtml.replace(/!LEVEL!/, json_res.vulnerabilities[ent].level);
-                tableHtml = tableHtml+tableItemHtml;
-          
-                // console.log(details)
-                // console.log(FILEPATH)
-                fs.writeFile(FILEPATH, details, function (err) {
-                    if (err)
-                        throw err;
-                });
-
-                let range = document.lineAt(json_res.vulnerabilities[ent].lineNo[0]-1).range;
-                
-                let severity : any;
-                severity = json_res.vulnerabilities[ent].level;
-                if (severity === 'error') {
-                    severity = vscode.DiagnosticSeverity.Error;
-                }
-                else {
-                    severity = vscode.DiagnosticSeverity.Warning;
-                }
-                // severity = vscode.DiagnosticSeverity.Error;
-                // let relatedInformation = ''
-                let diagnostic = new vscode.Diagnostic(range, message, severity);
-                diagnostics.push(diagnostic);
-            }
-        }
-
-        tableHtml = tableHtml + tableSuffix;
-        detailHtml = detailHtml + detailSuffix;
-        let html = tableHtml + detailHtml;
-        fs.writeFile(htmlPath, html, function (err) {
-            if (err)
-                throw err;
-        });
-
-        console.log("Security Analyze With Complier Finish.");
-        collection.set(document.uri, diagnostics);
-	} else {
-		collection.clear();
-	}
-}
---
+++ src/utils/generateReports.ts
@@ -0,0 +1,116 @@
+import * as vscode from 'vscode'
+import fs = require('fs')
+import { detailItem, detailPrefix, detailSuffix, tablePrefix, tableSuffix, tableItem } from '../commands/ReportTemplate';
+
+
+const os = require('os')
+const { window } = vscode
+
+// Windows OS hack
+export function checkPlatform(
+    FILEPATH: string
+): string {
+    if (os.platform() === 'win32') {
+        FILEPATH = FILEPATH.replace(/\\/g, '/')
+        if (FILEPATH.charAt(0) === '/') {
+            FILEPATH = FILEPATH.substr(1)
+        }
+    }
+    return FILEPATH;
+}
+
+export function updateDiagnostics(document: vscode.TextDocument | undefined, collection: vscode.DiagnosticCollection, obj:any, fileUri:any): void {
+    let diagnostics: vscode.Diagnostic[] = [];
+    let FILEPATH = checkPlatform(fileUri.fsPath);
+    
+    // console.log('FILEPATH  '+FILEPATH)
+    let htmlPath = FILEPATH;
+    let datetime = new Date();
+    var y = datetime.getFullYear();
+    var m = datetime.getMonth() + 1;
+    var d = datetime.getDate();
+    var h = datetime.getHours();
+    var mm = datetime.getMinutes();
+    var s = datetime.getSeconds();
+    let dateString = y.toString() + m.toString() + d.toString() + '_' + h.toString() + mm.toString() + s.toString();
+    let reportFolder = FILEPATH.substring(0, FILEPATH.lastIndexOf('/')) + '/reports/';
+    FILEPATH = reportFolder + 'vulnerabilitiesInfo_' + dateString + '.txt';
+    htmlPath = reportFolder + 'vulnerabilitiesReport_' + dateString + '.html';
+    if (document) {
+        // console.log(document.uri)
+        vscode.languages.getDiagnostics(document.uri).slice(1,1);
+        obj = JSON.parse(JSON.stringify(obj.text));
+        let json_res = JSON.parse(obj);
+        console.log(json_res);
+        
+        // the HTML page
+        let detailHtml = detailPrefix;
+        let tableHtml = tablePrefix;
+
+        for(var ent in json_res.vulnerabilities) {
+            let respos = json_res.vulnerabilities[ent];
+            if(respos) {
+                let message = `Name: ` + respos.name;
+                // write the detail information into the file
+                let details = `Name:` + respos.name + '\n' +
+                    `Description:` + respos.description + '\n' +
+                    `SwcID:` + respos.swcId + '\n' +
+                    `Advice:` + respos.advice + '\n' +
+                    `Line:` + respos.lineNo[0] + '\n' +
+                    `Level:` + respos.level + '\n' + '\n';
+
+                // Generate the detail html sector of the current vulnerability
+                let curDetail=detailItem.replace(/!NAME!/, respos.name);
+                curDetail = curDetail.replace(/!DESCRIPTION!/, respos.description);
+                curDetail = curDetail.replace(/!SWCID!/, respos.swcId);
+                curDetail = curDetail.replace(/!ADVICE!/, respos.advice);
+                curDetail = curDetail.replace(/!LINE!/, respos.lineNo[0]);
+                curDetail = curDetail.replace(/!LEVEL!/, respos.level);
+                curDetail = curDetail.replace(/SOURCECODE/, document.lineAt(respos.lineNo[0]-1).text);
+                detailHtml = detailHtml + curDetail;
+
+                // Generate the table html sector of the current vulnerability
+                let tableItemHtml = tableItem.replace(/!NAME!/, respos.name);
+                tableItemHtml = tableItemHtml.replace(/!LINE!/, respos.lineNo[0]);
+                tableItemHtml = tableItemHtml.replace(/!LEVEL!/, respos.level);
+                tableHtml = tableHtml+tableItemHtml;
+          
+                // console.log(details)
+                // console.log(FILEPATH)
+                fs.writeFile(FILEPATH, details, function (err) {
+                    if (err)
+                        throw err;
+                });
+
+                let range = document.lineAt(respos.lineNo[0]-1).range;
+                
+                let severity : any;
+                severity = respos.level;
+                if (severity === 'error') {
+                    severity = vscode.DiagnosticSeverity.Error;
+                }
+                else {
+                    severity = vscode.DiagnosticSeverity.Warning;
+                }
+                // severity = vscode.DiagnosticSeverity.Error;
+                // let relatedInformation = ''
+                let diagnostic = new vscode.Diagnostic(range, message, severity);
+                diagnostics.push(diagnostic);
+            }
+        }
+
+        tableHtml = tableHtml + tableSuffix;
+        detailHtml = detailHtml + detailSuffix;
+        let html = tableHtml + detailHtml;
+        fs.writeFile(htmlPath, html, function (err) {
+            console.log(htmlPath)
+            if (err)
+                throw err;
+        });
+
+        console.log("Security Analyze With Complier Finish.");
+        collection.set(document.uri, diagnostics);
+	} else {
+		collection.clear();
+	}
+}
