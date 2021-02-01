5aa3a2d2c5817f3a37d50678c23b09793f4767c3
Chenxi Li <lichenxi.webank@gmail.com>
2021-02-01 20:24:06 +0800

    remove useless file

--- backend/secure_check/backdoor_detector/MadMax/TODO.bash
+++
@@ -1,7 +0,0 @@
-cd output
-rm -rf ./*
-# compile
-../solc4.26 --bin-runtime ../examples/loop.sol | tail -n 1 > ../examples/loop.hex
-../bin/decompile -n -v -c "remove_unreachable=1" -g ../examples/loop.html ../examples/loop.hex
-# datalog analysis
-../bin/analyze.sh ../examples/loop.hex ../tools/bulk_analyser/mySpec.dl
